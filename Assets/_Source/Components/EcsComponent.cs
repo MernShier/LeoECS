b4d3901e19f3a8d86606d41a148750b971d513b1
MernShier <74783217+MernShier@users.noreply.github.com>
2024-03-06 09:51:09 +0300

    Initial commit

---
+++ Assets/_Source/Components/EcsComponent.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Client 
+{
+    public struct EcsComponent
+    {
+        public float Counter;
+        public Transform Anchor;
+    }
+}
---
+++ Assets/_Source/Systems/EcsInitSystem.cs
@@ -0,0 +1,16 @@
+using Leopotam.EcsLite;
+
+namespace Client {
+    sealed class EcsInitSystem : IEcsInitSystem, IEcsRunSystem
+    {
+        public void Init (IEcsSystems systems) 
+        {
+            
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            
+        }
+    }
+}
